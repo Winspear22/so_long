4ace27ae93f67c048459aa1a8f31b5a990bbc6ee
Winspear22 <adnen.aloui2@hotmail.com>
2024-04-10 14:40:49 +0200

    so_long : plus de soucis avec le nombre de lignes le calloc et le gnl ont bien été gérés, les closes aussi, pas de leak.

--- srcs/get_map_content.c
+++ srcs/get_map_content.c
@@ -14,10 +14,44 @@ int	check_get_next_line(char *line)
 	return (FAILURE);
 }
 
+int		get_number_of_lines(char *s)
+{
+	int i;
+	int fd2;
+	char *line;
+
+	i = 0;
+	fd2 = open(s, O_RDONLY);
+	if (fd2 < 0)
+	{
+		ft_printf("%s", "Error. Open map.ber failed.");
+		return (FAILURE);
+	}
+	line = get_next_line(fd2);
+	while (line != NULL)
+	{
+		if (check_get_next_line(line) == SUCCESS)
+		{
+			i++;
+		}
+		free(line);
+		line = get_next_line(fd2);
+	}
+	close(fd2);
+	printf("%s%d", "number of line in file = ", i);
+	return (i);
+}
+
+
 t_map	*reduce_get_map(t_data *data, int fd)
 {
 	char	*line;
 	int		i;
+
+	// Allouer suffisamment d'espace pour stocker toutes les lignes du fichier
+	data->map->map = ft_calloc(data->map->count_line + 1, sizeof(char *));
+	if (!(data->map->map))
+		return (NULL);
 
 	i = 0;
 	line = get_next_line(fd);
@@ -25,8 +59,9 @@ t_map	*reduce_get_map(t_data *data, int fd)
 	{
 		if (check_get_next_line(line) == SUCCESS)
 		{
-			if (i <= 5)
+			if (i < data->map->count_line)
 			{
+				// Copier la ligne dans le tableau
 				data->map->map[i] = strdup_no_n(line);
 				i++;
 			}
@@ -38,6 +73,7 @@ t_map	*reduce_get_map(t_data *data, int fd)
 	close(fd);
 	return (data->map);
 }
+
 
 t_vector	get_map_size(t_data *data)//char **full_map, t_vector size)
 {
@@ -64,9 +100,6 @@ t_map	*get_map(char *s, t_data *data)
 {
 	int		fd;
 
-	data->map->map = ft_calloc(6 + 1, sizeof(char *));
-	if (!(data->map->map))
-		return (NULL);
 	fd = open(s, O_RDONLY);
 	if (fd < 0)
 	{
@@ -75,6 +108,9 @@ t_map	*get_map(char *s, t_data *data)
 	}
 	else
 	{
+		data->map->count_line = get_number_of_lines(s);
+		if (data->map->count_line == FAILURE)
+			return (NULL);
 		data->map = reduce_get_map(data, fd);
 		data->map->map_max_size = get_map_size(data);
 	}
