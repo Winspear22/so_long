a7c738cff33e2109e2149022058ef00ad5bf6599
Winspear22 <adnen.aloui2@hotmail.com>
2024-04-19 15:31:37 +0200

    so_long -- Projet presque terminé, il faut paufiner les détails et nettoyer le code.

--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,75 @@
 
 # include "so_long.h"
 
+int	ft_count_items(char **tab)
+{
+	int	i;
+	int	j;
+	int	items;
+
+	i = 0;
+	j = 0;
+	items = 0;
+	while (tab[i])
+	{
+		j = 0;
+		while (tab[i][j])
+		{
+			if (tab[i][j] == 'C')
+				items += 1;
+			j++;
+		}
+		i++;
+	}
+	return (items);
+}
+
+t_img	ft_new_sprite(void *mlx, char *path)
+{
+	t_img	img;
+
+	img.img = 0;
+	img.img = mlx_xpm_file_to_image(mlx, path, &img.width, &img.height);
+	img.addr = mlx_get_data_addr(img.img,
+			&img.bpp, &img.line, &img.endian);
+	return (img);
+}
+
+int	close_redx(t_data *data)
+{
+	if (data->win_ptr)
+	{
+		free_data(data);
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		mlx_destroy_display(data->mlx_ptr);
+		data->win_ptr = NULL;
+	}
+	free(data->mlx_ptr);
+	if (data != NULL)
+		free(data);
+	exit(SUCCESS);
+}
+
+void	ft_take(char **tab, t_data *data)
+{
+	int	items;
+
+	items = 0;
+	items = ft_count_items(tab);
+	if (data->map->map[data->map->player.p_pos.y][data->map->player.p_pos.x] == 'C')
+	{
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->floor_texture.img,
+   	 		data->map->player.p_pos.y, data->map->player.p_pos.x);
+		data->map->map[data->map->player.p_pos.y][data->map->player.p_pos.x] = '0';
+		items -= 1;
+	}
+	if (data->map->map[data->map->player.p_pos.y][data->map->player.p_pos.x] == 'E' && items == 0)
+	{
+		write(1, "You finished the game, congrats !\n", 35);
+		close_redx(data);
+	}
+}
+
 t_vector	ft_player_pos(char **tab)
 {
 	int			i;
@@ -24,17 +93,6 @@ t_vector	ft_player_pos(char **tab)
 		i++;
 	}
 	return (player_pos);
-}
-
-t_img	ft_new_sprite(void *mlx, char *path)
-{
-	t_img	img;
-
-	img.img = 0;
-	img.img = mlx_xpm_file_to_image(mlx, path, &img.width, &img.height);
-	img.addr = mlx_get_data_addr(img.img,
-			&img.bpp, &img.line, &img.endian);
-	return (img);
 }
 
 int	ft_draw_walls(void *mlx, void *window, int k, int l, t_data *data)
@@ -89,7 +147,6 @@ int	ft_create_character(void *mlx, void *window, int k, int l, t_data *data)
 	}
 	else
 	{
-		//if (data->character_texture.img == NULL)
     	data->character_texture = ft_new_sprite(mlx, "sprites/gokuu.xpm"); 
 		mlx_put_image_to_window(mlx, window, data->floor_texture.img,
    	 		k, l);
@@ -98,7 +155,7 @@ int	ft_create_character(void *mlx, void *window, int k, int l, t_data *data)
     return (k);
 }
 
-int	ft_draw(t_data *data)//(char **tab, void *mlx, void *window, t_data *data)
+int	ft_draw(t_data *data)
 {
 	t_check_map	map;
 
@@ -121,28 +178,11 @@ int	ft_draw(t_data *data)//(char **tab, void *mlx, void *window, t_data *data)
 			else if (data->map->map[map.i][map.j] == 'P')
 				map.k = ft_create_character(data->mlx_ptr, data->win_ptr, map.k, map.l, data);
 		}
-
 		map.j = -1;
 		map.l = map.l + 100;
 		map.k = 0;
 	}
 	return SUCCESS;
-}
-
-int	close_redx(t_data *data)
-{
-	if (data->win_ptr)
-	{
-		//free_map_info(data->map_info, data);
-		free_data(data);
-		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-		mlx_destroy_display(data->mlx_ptr);
-		data->win_ptr = NULL;
-	}
-	free(data->mlx_ptr);
-	if (data != NULL)
-		free(data);
-	exit(SUCCESS);
 }
 
 int	press_keyboard(int key, t_data *data)
@@ -183,46 +223,15 @@ int	release_keyboard(int key, t_data *data)
 	return (SUCCESS);
 }
 
-/*int	ft_move_player(int key, t_data *data)
+void	ft_refresh_character(t_data *data)
 {
-	//t_program	*i;
-	int			j;
-
-	//i = (t_program *)param;
-	//j = ft_count_movements(*i, key);
-	if (key == 100 && i->tab[i->pos.y][i->pos.x + 1] != '1')
-		i->pos.x += 1;
-	else if (key == 97 && i->tab[i->pos.y][i->pos.x - 1] != '1')
-		i->pos.x -= 1;
-	else if (key == 115 && i->tab[i->pos.y + 1][i->pos.x] != '1')
-		i->pos.y += 1;
-	else if (key == 119 && i->tab[i->pos.y - 1][i->pos.x] != '1')
-		i->pos.y -= 1;
-	ft_refresh_character("sprites/gokuu.xpm", *i);
-	ft_take(i->tab, *i);
-	if (key == 65307)
-		exit(0);
-	mlx_string_put(i->mlx, i->w.ref, 50, 50, 15921105, ft_itoa(j));
-	return (0);
-}*/
-
-void	ft_refresh_character(char *path, t_data *data)
-{
-	(void)path;
 	ft_draw(data);
-	//data->character_texture = ft_new_sprite(data->mlx_ptr, path);
-	printf("%s%d\n", "ICIIIIIIIIIIIIIIIIIIIIII x", data->map->player.p_pos.x);
-	printf("%s%d\n", "ICIIIIIIIIIIIIIIIIIIIIII y", data->map->player.p_pos.y);
-
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
 		data->character_texture.img, data->map->player.p_pos.x * 100, data->map->player.p_pos.y * 100);
 }
 
 int ft_move_player(int key, t_data *data)
 {
-	printf("%s%d\n", "key ==== ", key);
-	printf("%s%d\n", "x ==== ", data->map->player.p_pos.x);
-	printf("%s%d\n", "y ==== ", data->map->player.p_pos.y);
 	if (key == 100 && data->map->map[data->map->player.p_pos.y][data->map->player.p_pos.x + 1] != '1')
 		data->map->player.p_pos.x += 1;
 	if (key == 97 && data->map->map[data->map->player.p_pos.y][data->map->player.p_pos.x - 1] != '1')
@@ -233,22 +242,19 @@ int ft_move_player(int key, t_data *data)
 		data->map->player.p_pos.y -= 1;
 	if (key == XK_Escape)
 		close_redx(data);
-	//free_data(data);
 	if (data->character_texture.img != NULL)
 		mlx_destroy_image(data->mlx_ptr, data->character_texture.img);
-	ft_refresh_character("sprites/gokuu.xpm", data);
+	ft_refresh_character(data);
+	ft_take(data->map->map, data);
 	return SUCCESS;
 
 }
 
 int loop(t_data *data)
 {
-    //ft_draw(data->map->map, data->mlx_ptr, data->win_ptr, data);
-	//mlx_loop_hook(data->mlx_ptr, ft_draw, data);
 	mlx_hook(data->win_ptr, 2, 1L << 0, &press_keyboard, data);
 	mlx_hook(data->win_ptr, 3, 1L << 1, &release_keyboard, data);
 	mlx_key_hook(data->win_ptr, *ft_move_player, data);
-
     mlx_hook(data->win_ptr, 17, 0L, &close_redx, data);
 	mlx_loop(data->mlx_ptr);
 	return (SUCCESS);
